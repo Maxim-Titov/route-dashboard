959c9e92fc3aeab017e438b0c1b374f5431a639e
Maxim-Tytov <tutovmaksum01@gmail.com>
2026-05-13 00:14:12 +0300

    [fix] Fixed error with getting data of trips

--- back/main.py
+++ back/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://alfred-phone-bridge.onrender.com"],
+    # allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_origins=["https://autoservicetourdashboard.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
