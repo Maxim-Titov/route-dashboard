08d7e7e5e533273311c16a06b3bfebdeafd03ce3
Maxim-Tytov <tutovmaksum01@gmail.com>
2026-05-13 17:18:54 +0300

    [fix] Fixed database connection handling in various modules to use try-finally for cursor and connection closure

--- back/main.py
+++ back/main.py
@@ -18,6 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
+    # allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_origins=["https://autoservicetourdashboard.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
